b23633ed2de394a7d57b3f3d69a84eb359bdf389
Ivan Marcano <ijmm15@gmail.com>
2026-03-19 17:05:18 -0500

    crea paquete

---
+++ ejercicio_1/main.py
@@ -0,0 +1,8 @@
+from paquete_test.prueba_de_git import *
+
+
+def  main():
+    nro = int(input("nro: "))
+    imprime_la_prueba(nro)
+
+main()
---
+++ ejercicio_1/paquete_test/prueba_de_git.py
@@ -0,0 +1,2 @@
+def imprime_la_prueba(nro: int)-> None:
+    print(f"este es una prueba del paquete nro: {nro}")
